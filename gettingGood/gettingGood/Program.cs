8cda2d3f6df7b4538bd33a62173e338fad22cc29
DNIIBOY <daniel.nettelfield@gmail.com>
2021-08-09 10:47:11 +0200

    Initial Commit

---
+++ gettingGood/gettingGood/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gettingGood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
