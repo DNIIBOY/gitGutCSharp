b268c904dd3c88de4758e9e2b09fede40f64ad22
DNIIBOY <daniel.nettelfield@gmail.com>
2021-08-17 09:22:11 +0200

    Create WPF and stuff

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -6,7 +6,11 @@ namespace Tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x = 7.2;
+            char y = 'G';
+            int z = 101;
+            // 1054 : int, -522 : int, 1.234 : double, k : char/string
+            // -4.3 : int, y : char/string, 19488 : int
         }
     }
 }
