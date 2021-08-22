7751b2060547565fb5fea712317399f07eed9bdb
DNIIBOY <daniel.nettelfield@gmail.com>
2021-08-22 22:33:35 +0200

    V10 test file

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -7,12 +7,28 @@ namespace Tests
     {
         static void Main(string[] args)
         {
-            int x = 4;
-            int y = 3;
+            int x = 42;
+            int y = 64;
 
-            string output = "x er større end y? ";
-            output += x > y;
-            Console.WriteLine(output);
+            if (x + y > 100)
+            {
+                Console.WriteLine("Summen er større end 100!");
+            }
+            else
+            {
+                Console.WriteLine("Summen er mindre end 100");
+            }
+
+
+            bool result = x + y > 100;
+            if (result)
+            {
+                Console.WriteLine("Summen er større end 100!");
+            }
+            else
+            {
+                Console.WriteLine("Summen er mindre end 100");
+            }
         }
     }
 }
