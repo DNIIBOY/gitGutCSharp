59d238b55f91e86338e339893ce2f9ae920a5b09
DNIIBOY <daniel.nettelfield@gmail.com>
2021-10-01 13:42:59 +0200

    V21

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -2,34 +2,18 @@
 
 namespace Tests
 {
-    class Person
-    {
-        private int _age;
-        private string _name;
-        private string _hairColor;
-
-        public Person(int age, string name, string hairColor)
-        {
-            _age = age;
-            _name = name;
-            _hairColor = hairColor;
-        }
-        public Person (string name) : this(-1, name, "undefined"){}
-
-        public void PrintInfo()
-        {
-            Console.WriteLine($"Jeg heder {_name}, jeg er {_age} år gammel og mit hår er {_hairColor}");
-        }
-        
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person("Bob");
-            Person p2 = new Person(15, "Karen", "Blåt");
-            p1.PrintInfo();
-            p2.PrintInfo();
+            int[] x = { -2, -1, 0, 10 };
+            int[] y = new int[4];
+            y[0] = -2;
+            y[1] = -1;
+            y[2] = 0;
+            y[3] = 10;
+            Console.WriteLine(x[1]);
+            Console.WriteLine(y[3]);
         }
     }
 }
