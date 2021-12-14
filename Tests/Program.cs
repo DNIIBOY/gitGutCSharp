5bee7d37b73204edf3dc52dfb39b5426a5060805
DNIIBOY <daniel.nettelfield@gmail.com>
2021-12-14 12:52:28 +0100

    Opg 3

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -6,24 +6,20 @@ namespace Tests
 {
     class Program
     {
-        static void Main(string[] args){
-            for (int i = 0; i < 3; i++){
-                try{
-                    int user = int.Parse(Console.ReadLine());
-                    int pass = int.Parse(Console.ReadLine());
-                    
-                    if (user == 12 && pass == 1234){
-                        break;
-                    }
-                    else{
-                        throw new FormatException();
-                    }
-                }
-                catch (FormatException){
-                    Console.WriteLine("Login Failed");
+        static public int countSpaces(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count++;
                 }
             }
-            Console.WriteLine("Login Successful");
+            return count;
+        }
+        static void Main(string[] args){
+            Console.WriteLine(countSpaces(Console.ReadLine()));
         }
     }
 }
