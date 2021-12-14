dccede94c4d0865e94aef8244a88b89a7f19abb9
DNIIBOY <daniel.nettelfield@gmail.com>
2021-12-14 12:33:13 +0100

    Opg 1.1.5

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -10,17 +10,16 @@ namespace Tests
             int num = int.Parse(Console.ReadLine());
             int H = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            
-            for (int i = H; i > 0; i--)
-            {
-                for (int k = 0; k < H-i; k++){
-                    Console.Write(" ");
-                }
-                for (int j = 0; j < i; j++){
-                    Console.Write(num);
-                }
-                Console.WriteLine();
+
+            for (int i = 0; i < H - 1; i++){
+                Console.Write(" ");
             }
+            Console.WriteLine(num);
+            
+            for (int i = 1; i < H-1; i+=2){
+                
+            }
+            
         }
     }
 }
