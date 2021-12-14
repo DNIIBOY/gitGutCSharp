c5b39932dbccb7f96c59a98b42e60c20a486800a
DNIIBOY <daniel.nettelfield@gmail.com>
2021-12-14 12:50:06 +0100

    Opg 2

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -7,19 +7,23 @@ namespace Tests
     class Program
     {
         static void Main(string[] args){
-            int num = int.Parse(Console.ReadLine());
-            int H = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-
-            for (int i = 0; i < H - 1; i++){
-                Console.Write(" ");
+            for (int i = 0; i < 3; i++){
+                try{
+                    int user = int.Parse(Console.ReadLine());
+                    int pass = int.Parse(Console.ReadLine());
+                    
+                    if (user == 12 && pass == 1234){
+                        break;
+                    }
+                    else{
+                        throw new FormatException();
+                    }
+                }
+                catch (FormatException){
+                    Console.WriteLine("Login Failed");
+                }
             }
-            Console.WriteLine(num);
-            
-            for (int i = 1; i < H-1; i+=2){
-                
-            }
-            
+            Console.WriteLine("Login Successful");
         }
     }
 }
