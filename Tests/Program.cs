6e7630721da5b251e9db5e29a0f4b6d204b8022b
DNIIBOY <daniel.nettelfield@gmail.com>
2021-12-14 12:57:32 +0100

    Opg 4

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Tests
 {
     class Program
     {
-        static public int countSpaces(string str)
-        {
-            int count = 0;
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (str[i] == ' ')
-                {
-                    count++;
+        static void Main(string[] args){
+            List<int> pos = new List<int>();
+            List<int> neg = new List<int>();
+            for (int i = 0; i < 10; i++){
+                int input = int.Parse(Console.ReadLine());
+                if (input >= 0){
+                    pos.Add(input);
+                }
+                else{
+                    neg.Add(input);
                 }
             }
-            return count;
-        }
-        static void Main(string[] args){
-            Console.WriteLine(countSpaces(Console.ReadLine()));
+
+            Console.WriteLine($"Avg Positive: {pos.Average()}");
+            Console.WriteLine($"Avg Negative: {neg.Average()}");
         }
     }
 }
